777c6cef8999d79651495acf4773e3155992c417
Cynthia Palma <cpalmah.187@gmail.com>
2019-03-18 10:37:25 +0100

    Add tests for searchFood method

--- app/src/test/java/com/test/abc/ui/main/MainViewModelKTest.kt
+++ app/src/test/java/com/test/abc/ui/main/MainViewModelKTest.kt
@@ -3,6 +3,7 @@ package com.test.abc.ui.main
 import android.view.View
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.test.abc.data.FoodRepository
+import com.test.abc.data.remote.Food
 import com.test.abc.data.remote.FoodResponse
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.schedulers.Schedulers
@@ -10,15 +11,20 @@ import io.reactivex.subjects.PublishSubject
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
 
 class MainViewModelKTest {
 
-    lateinit var foodRepository: FoodRepository
+    @Mock
+    private lateinit var foodRepository: FoodRepository
+    private lateinit var sut: MainViewModel
 
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
@@ -26,42 +32,67 @@ class MainViewModelKTest {
     @Before
     fun setUp() {
         RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
-        foodRepository = mock(FoodRepository::class.java)
+        MockitoAnnotations.initMocks(this);
+        sut = MainViewModel(foodRepository)
     }
 
     @Test
     fun searchFood_subscribe() {
         // Arrange
-        val sut = MainViewModel(foodRepository)
         val query = "Tacos"
 
-        val subject = PublishSubject.create<FoodResponse>()
-//        doReturn(subject).`when`(foodRepository).searchFood(query)
-        `when`(foodRepository.searchFood(query)).thenReturn(subject)
+        val searchFoodObservable = PublishSubject.create<FoodResponse>()
+        `when`(foodRepository.searchFood(query)).thenReturn(searchFoodObservable)
 
         // Act
         sut.searchFood(query)
+        val testObserver = searchFoodObservable.test()
 
         // Assert
         assertThat(sut.loading.value, `is`(View.VISIBLE))
-        val testObserver = subject.test()
-        testObserver.assertEmpty()
-        testObserver.onNext(FoodResponse())
-        testObserver.assertEmpty()
-//        testObserver.assertSubscribed()
-//        assertThat(subject.test().hasSubscription(), `is`(true))
+        testObserver.assertSubscribed()
+        testObserver.assertNoValues()
     }
 
     @Test
-    fun saveFood() {
+    fun searchFood_onNext() {
+        // Arrange
+        val query = "Tacos"
+        val foodResponse = mock(FoodResponse::class.java)
+        val foodList = ArrayList<Food>()
+
+        `when`(foodResponse.foodList).thenReturn(foodList)
+        val searchFoodObservable = PublishSubject.create<FoodResponse>()
+        `when`(foodRepository.searchFood(query)).thenReturn(searchFoodObservable)
+
+        // Act
+        sut.searchFood(query)
+        val testObserver = searchFoodObservable.test()
+        searchFoodObservable.onNext(foodResponse)
+
+        // Assert
+        testObserver.assertValue(foodResponse)
+        assertEquals(sut.searchResult.value, foodList)
+        assertThat(sut.loading.value, `is`(View.INVISIBLE))
     }
 
     @Test
-    fun getAllSavedFood() {
-    }
+    fun searchFood_onError() {
+        // Arrange
+        val query = "Tacos"
+        val error = java.lang.Exception()
 
-    @Test
-    fun onCleared() {
+        val searchFoodObservable = PublishSubject.create<FoodResponse>()
+        `when`(foodRepository.searchFood(query)).thenReturn(searchFoodObservable)
+
+        // Act
+        sut.searchFood(query)
+        val testObserver = searchFoodObservable.test()
+        searchFoodObservable.onError(error)
+
+        // Assert
+        testObserver.assertError(error)
+        assertThat(sut.loading.value, `is`(View.INVISIBLE))
     }
 
     @After
